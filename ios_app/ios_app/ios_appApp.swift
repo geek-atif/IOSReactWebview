9a574ba2e5c9298a284d96bcc88cfa7a983c329d
Atif Qamar <atif.q@ezetap.com>
2024-01-26 21:41:15 +0530

    project created

---
+++ ios_app/ios_app/ios_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_appApp.swift
+//  ios_app
+//
+//  Created by Atif Qamar on 26/01/24.
+//
+
+import SwiftUI
+
+@main
+struct ios_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
